6a08a1a6f5c42ac3ff0e3fb336e343be78fa573f
SimoCrash <simone.crescenzi@hotmail.com>
2022-10-26 16:24:39 +0200

    Milestone 2

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const arrImg = [
+    "img/01.jpg",
+    "img/02.jpg",
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg",
+] 
+
+const sliderImg = document.querySelector(".container");
+const btnTop = document.querySelector(".btn-top");
+const btnBottom = document.querySelector(".btn-bottom");
+const firstImg = 0;
+
+for (let i = 0; i < arrImg.length; i++) {
+    const eleImg = document.createElement("img");
+    eleImg.src = arrImg[i];
+    eleImg.classList.add("slider-img");
+
+    if (i === 0) {
+        eleImg.classList.add("d-block");
+    }
+    sliderImg.append(eleImg);
+}
